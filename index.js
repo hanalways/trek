6d56eb29e2040c2ba8029285cfa6ff2727631569
Hana <hanalways@gmail.com>
2019-05-30 08:49:46 -0700

    JS scaffolding code

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-// Loading Treks
-const loadTreks = () => {
-  
+const displayStatus = (message) => {
+  $('#status').html(message);
 }
+
+const handleApiError = (error) => {
+  console.log(error);
+  // TODO: politely report this error to the user
+}
+
+const loadTrips = () => {
+  displayStatus("loading trips...");
+
+  // TODO: Wave 1
+  // make an axios call to the trips index and display the results
+}
+
+const showTripDetails = (trip) => {
+  console.log("showing details for trip", trip);
+
+  // TODO: Wave 2
+  // display trip details and the trip reservation form
+}
+
+const reserveTrip = (trip) => {
+  console.log("reserving trip", trip)
+
+  // TODO: Wave 2
+  // reserve a spot on the trip when the form is submitted
+}
+
+$(document).ready(() => {
+  $('#load-trips').click(loadTrips);
+});
